008b5212cfbfe73779455d887aba447f8c473f91
Victorio <benjaminvictorio2018@gmail.com>
2020-12-31 12:34:41 +0100

    arreglado creo que ahora si

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ client.on("message", function(message) {
   message.reply(`repiola`);  
   //piola                         
   }                            
-  
 
-  
+
+
   else if (command === "a") {
   message.reply(`si a`);  
   //a                         
@@ -54,24 +54,24 @@ client.on("message", function(message) {
 
   else if (command === "tu") {
     message.reply(`preguntaste por mi o que si preeguntas por mi soy pancho del rencho y estoy en beta gracias por preguntar ${message.author}`)
-    
 
-    
+
+
   }
 
   else if (command === 'id') {
     message.reply (`tu id es ${message.author.id}`)
   }
-  
-  
+
+
   else if (command === "gg") {
   message.channel.send(`bien jugado ${message.author}`);  
   //gg                    
 
   } 
-  
-  
-  
+
+
+
 
 
   else if (command === "f") {
@@ -79,14 +79,14 @@ client.on("message", function(message) {
   //f
   }
 
-  
-  
+
+
   else if (command === 'meme') {
     var meme = ['https://media.discordapp.net/attachments/757716017618223215/784488476736749588/EJlcnPcWsAEtkQk.png','https://media.discordapp.net/attachments/757548223660556300/785200403959513123/image0.png?width=854&height=641','https://media.discordapp.net/attachments/757716017618223215/784488476518776852/EKIeT1eUcAA5_Q3.jpeg?width=571&height=641','https://media.discordapp.net/attachments/757716017618223215/784488476343009300/20200513_183249.jpg','https://media.discordapp.net/attachments/757716017618223215/784488475478589510/1606711309-5fc4780d6a9e6.jpg','https://media.discordapp.net/attachments/410197118263754753/711096608715964536/FB_IMG_1589586420725.jpg?width=610&height=641','https://media.discordapp.net/attachments/757406384651501678/785443810083602442/a_sos_re_troll.PNG?width=642&height=641','https://media.discordapp.net/attachments/753193857092419584/785119269238079498/IMG_20200720_190125.jpg?width=835&height=641','https://media.discordapp.net/attachments/759027747409494016/783401125281136690/13383059d47d6a3f545e73f9ed21ec0c.png',
     'https://cdn.discordapp.com/attachments/757548223660556300/785377156825743360/20201206_213852.jpg','https://media.discordapp.net/attachments/777686401000800288/783695980175425566/20200513_183249.jpg','https://media.discordapp.net/attachments/757406384651501678/782797537349599232/127184779_3439967339405268_9090584373566259655_n.jpg','https://media.discordapp.net/attachments/753193857092419584/786609706255908864/22ca504.jpg','https://media.discordapp.net/attachments/300761624225251348/786594177970470912/gamerngyes.jpg?width=360&height=640','https://media.discordapp.net/attachments/600241793396899866/785238177702215730/Screenshot_20201205-174556_Instagram.jpg?width=596&height=640','https://media.discordapp.net/attachments/515196838400229394/785867157346058250/image0.jpg','https://media.discordapp.net/attachments/694634173268230174/786463032565563400/image0-18-1.jpg','https://media.discordapp.net/attachments/694634173268230174/786607891858587658/FB_IMG_1607612304928.jpg','']
       message.channel.send (meme[Math.floor(Math.random() * meme.length)])
     }
-  
+
   else if (command === "help") {
   message.reply('mis comandos son : gg, f, a, invite, piola, sum, ping');
   //help
@@ -96,12 +96,12 @@ client.on("message", function(message) {
     message.channel.send(`tu eres  ${message.author}`);
     //help
     }
-  
+
   else if (command === "vizcarra") {
   message.reply('https://cdn.discordapp.com/attachments/757548223660556300/777529430947528704/video0.mp4');
   //
   }
-  
+
   else if (command === 'invite') {
     message.channel.send ('con esto podras invitarme a tu servidor https://botpiola.glitch.me/')
 
@@ -158,15 +158,15 @@ else if (command === "ban") {
 
   // invitcaion de bot https://discord.com/oauth2/authorize?client_id=%20776106257597333515&scope=bot&permissions=8
 
-  
+
 else if (command === 'server') {
   message.channel.send(`El nombre del servidor es: ${message.guild.name}\nTotal de miembros: ${message.guild.memberCount}\n se creo el ${message.guild.createdAt}\n Region:${message.guild.region}`);
 }
 
-  
-  
+
+
   });  
-  
+
 
   client.on('message', message  => {
 if (message.content === 'que') {
@@ -196,14 +196,13 @@ if (message.content === `nose`) {
   .then
   message.channel.send(`yo tampoco`)
 }
-    
+
   }
-  
-  
+
+
   )
 
 
 
 //terminan los comandos
 client.login(config.BOT_TOKEN);
-
