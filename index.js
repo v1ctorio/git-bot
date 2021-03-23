57962b32394c35291e11eb77601ab47c5f5c2932
Victoriorepiola <benjaminvictorio2018@gmail.com>
2021-03-23 18:30:00 +0100

    add &pp

--- index.js
+++ index.js
@@ -92,7 +92,25 @@ message.channel.send(args.join(" "));
   //auditoria
   
 
+else if (command === 'pp') {
+ let miembro = message.mentions.users.first()
+if (!miembro) {
+    const embed = new Discord.MessageEmbed()
+        .setImage(`${message.author.displayAvatarURL()}`)
+        .setColor(0x66b3ff)
+        .setFooter(`Avatar de ${message.author.tag}`);
+    message.channel.send(embed);
 
+} else {
+    const embed = new Discord.MessageEmbed()
+        .setImage(`${miembro.displayAvatarURL()}`)
+        .setColor(0x66b3ff)
+        .setFooter(`Avatar de ${miembro.tag}`);
+
+    message.channel.send(embed);
+
+};
+}
 
   else if (command === "yo") {
     message.channel.send(`tu eres  ${message.author}`);
