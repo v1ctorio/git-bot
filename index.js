ce6d621d4fe6622da18e7a60660c03af61c7ccf8
Victoriorepiola <benjaminvictorio2018@gmail.com>
2021-04-12 15:41:49 +0200

    ph

--- index.js
+++ index.js
@@ -154,6 +154,25 @@ if (!miembro) {
         } // Fin
     }
 	
+else if (command === "phcomment"){ //Creamos el comando (esto lo adaptan a su codigo, claro)
+
+message.delete() //Esto es opcional. Es para borrar el mensaje que nosotros coloquemos como comando. si no lo quieren, borrenlo
+
+let txt = args.join('%20');  //Argumentos 
+
+if (!txt) return message.channel.send("Olvidaste colocar los argumentos.") //Si no hay argumentos...
+
+let autor = message.author; //Definimos autor
+
+
+
+let attachment = new Discord.MessageAttachment(`https://nekobot.xyz/api/imagegen?type=phcomment&image=${message.author.displayAvatarURL()}&text=${txt}&username=${autor.username}&raw=1`,'logo.png') //Pedimos la imagen
+
+
+
+message.channel.send(attachment)    //La enviamos
+
+}   
 
 
 else if (command === 'magik') {//abrimos cmd   
